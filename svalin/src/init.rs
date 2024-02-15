be48e9fa6e70a1a49a3f9fdec19cfc1799b8fee0
wrong <acul009@gmail.com>
2024-02-15 01:47:16 +0100

    partially working macro

--- svalin/src/init.rs
+++ svalin/src/init.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use svalin_macros::rpc_dispatch;
 use svalin_pki::Keypair;
 use svalin_rpc::{CommandHandler, Connection, Session, SessionOpen};
 
@@ -21,25 +21,21 @@ impl CommandHandler for InitHandler {
     }
 }
 
-#[rpc_dispatch("init_key")]
-async fn init(
-    session: Session<SessionOpen>,
-    initname: String,
-    moreargs: Foo,
-) -> Result<ReturnType> {
+#[rpc_dispatch(init_key)]
+async fn init(session: &Session<SessionOpen>, initname: String) -> Result<String> {
     todo!()
 }
 
-#[async_trait]
-trait InitDispatcher {
-    async fn init<'a>(&'a self, initname: String, moreargs: Foo) -> Result<ReturnType>;
-}
+// #[async_trait]
+// trait InitDispatcher {
+//     async fn init<'a>(&'a self, initname: String, moreargs: Foo) -> Result<ReturnType>;
+// }
 
-#[async_trait]
-impl InitDispatcher for Connection {
-    async fn init<'a>(&'a self, initname: String, moreargs: Foo) -> Result<ReturnType> {
-        let session = self.open_session("init_key".into()).await?;
+// #[async_trait]
+// impl InitDispatcher for Connection {
+//     async fn init<'a>(&'a self, initname: String, moreargs: Foo) -> Result<ReturnType> {
+//         let session = self.open_session("init_key".into()).await?;
 
-        init(session, initname, moreargs).await
-    }
-}
+//         init(session, initname, moreargs).await
+//     }
+// }
--- svalin_macros/src/lib.rs
+++ svalin_macros/src/lib.rs
@@ -1,14 +1,20 @@
-use proc_macro::TokenStream;
-use syn::{parse_macro_input, punctuated::Punctuated, Error, ItemFn, Meta, Signature};
+use proc_macro::TokenStream as ProcTokenStream;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::{quote, ToTokens};
+use syn::{
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    spanned::Spanned,
+    token::{Comma, SelfValue},
+    Error, FnArg, ItemFn, Meta, Pat, PatType, Receiver, Signature,
+};
 
 #[proc_macro_attribute]
-pub fn rpc_dispatch(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn rpc_dispatch(args: ProcTokenStream, input: ProcTokenStream) -> ProcTokenStream {
     let args = parse_macro_input!(args with Punctuated::<Meta, syn::Token![,]>::parse_terminated);
     let item = parse_macro_input!(input as ItemFn);
 
-    to_dispatcher(args, item);
-
-    todo!()
+    to_dispatcher(args, item).expect("should work...").into()
 }
 
 fn to_dispatcher(
@@ -34,5 +40,49 @@ fn to_dispatcher(
         ..
     } = sig;
 
-    todo!()
+    if asyncness == None {
+        return Ok(parse_quote! {compile_error!("an RPC-Dispatcher needs to be async.")});
+    }
+
+    let mut trait_name = ident.to_string();
+    trait_name.push_str("Dispatcher");
+    let trait_ident = Ident::new(&trait_name, Span::call_site());
+
+    let mut parameters: Punctuated<FnArg, Comma> = Punctuated::new();
+    let mut iter = inputs.iter();
+
+    let session = iter.next();
+
+    for param in iter {
+        parameters.push(param.to_owned())
+    }
+
+    let mut call_parameters: Punctuated<Box<Pat>, Comma> = Punctuated::new();
+    let mut iter = inputs.iter();
+    let _session = iter.next();
+
+    for param in iter {
+        match param {
+            FnArg::Receiver(_) => unreachable!(),
+            FnArg::Typed(pat) => call_parameters.push(pat.pat.to_owned()),
+        }
+    }
+
+    Ok(syn::parse_quote!(
+        async fn #ident(#inputs) {
+            #block
+        }
+
+        trait #trait_ident {
+            async fn #ident(&self, #parameters) ;
+        }
+
+        impl #trait_ident for svalin_rpc::Connection {
+            async fn #ident(&self, #parameters)  {
+                let session = self.open_session("init_key".into()).await?;
+
+                #ident(&session, #call_parameters).await
+            }
+        }
+    ))
 }
