3de6ba693db45dee7c9a918db82f3add711ae8e0
L. Wlcek <luca@it-rahn.de>
2024-08-16 16:55:17 +0200

    ok, this doesnt work yet

--- gui_flutter/rust/src/api/streamsink.rs
+++ gui_flutter/rust/src/api/streamsink.rs
@@ -1,27 +1,59 @@
-use anyhow::anyhow;
+pub use anyhow::anyhow;
 pub use anyhow::Result;
+pub use svalin::client::{device::Device, Client, FirstConnect, Init, Login};
+pub use tokio::sync::broadcast::Receiver;
 
-use crate::frb_generated::{SseEncode, StreamSink};
+use crate::frb_generated::StreamSink;
 
-pub trait ToStreamSink {
-    type T: Clone;
-
-    async fn streamsink(&mut self, sink: StreamSink<Self::T>) -> Result<()>;
-}
-
-impl<T> ToStreamSink for tokio::sync::broadcast::Receiver<T>
-where
-    T: Clone + SseEncode,
-{
-    type T = T;
-
-    async fn streamsink(&mut self, sink: StreamSink<Self::T>) -> Result<()> {
-        while let Ok(msg) = self.recv().await {
-            if let Err(err) = sink.add(msg) {
-                return Err(anyhow!(err));
+macro_rules! create_streamsink_converter {
+    ($t:ty) => {
+        paste::paste! {
+            pub async fn [<$t _receiver_into_streamsink>](
+                receiver: Receiver<$t>,
+                sink: StreamSink<$t>,
+            ) {
+                while let Ok(msg) = receiver.recv().await {
+                    if let Err(_err) = sink.add(msg) {
+                        return;
+                    }
+                }
             }
         }
-
-        Ok(())
-    }
+    };
 }
+
+create_streamsink_converter!(Device);
+
+// pub async fn DeviceListItem_receiver_into_streamsink(
+//     receiver: Receiver<DeviceListItem>,
+//     sink: StreamSink<DeviceListItem>,
+// ) {
+//     while let Ok(msg) = receiver.recv().await {
+//         if let Err(err) = sink.add(msg) {
+//             return;
+//         }
+//     }
+// }
+
+// pub trait ToStreamSink {
+//     type T: Clone;
+
+//     async fn streamsink(&mut self, sink: StreamSink<Self::T>) -> Result<()>;
+// }
+
+// impl<T> ToStreamSink for tokio::sync::broadcast::Receiver<T>
+// where
+//     T: Clone + SseEncode,
+// {
+//     type T = T;
+
+//     async fn streamsink(&mut self, sink: StreamSink<Self::T>) -> Result<()> {
+//         while let Ok(msg) = self.recv().await {
+//             if let Err(err) = sink.add(msg) {
+//                 return Err(anyhow!(err));
+//             }
+//         }
+
+//         Ok(())
+//     }
+// }
