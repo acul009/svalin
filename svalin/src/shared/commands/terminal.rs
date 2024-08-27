669cc7680b40173d56d8df36cf5d51f26e968419
L. Wlcek <luca@it-rahn.de>
2024-08-27 22:58:56 +0200

    failed to implement terminal command

--- svalin/src/shared/commands/terminal.rs
+++ svalin/src/shared/commands/terminal.rs
@@ -1,13 +1,18 @@
+use std::pin::pin;
+
 use anyhow::Result;
 use async_trait::async_trait;
+use futures::{select, FutureExt};
+use serde::{Deserialize, Serialize};
 use svalin_rpc::rpc::{command::CommandHandler, session::Session};
-use svalin_sysctl::pty::PtyProcess;
+use svalin_sysctl::pty::{PtyProcess, TerminalSize};
 use tokio::sync::mpsc;
 
+#[derive(Debug, Serialize, Deserialize)]
 enum TerminalPacket {
     Close,
-    Input(String),
-    Resize { cols: u16, rows: u16 },
+    Input(Vec<u8>),
+    Resize(TerminalSize),
 }
 
 pub struct RemoteTerminal {
@@ -28,12 +33,37 @@ impl CommandHandler for RemoteTerminalHandler {
     fn key(&self) -> String {
         remote_terminal_key()
     }
+
     async fn handle(&self, session: &mut Session) -> Result<()> {
         let size: TerminalSize = session.read_object().await?;
-        let pty = PtyProcess::shell(rows, cols)
+        let mut pty = PtyProcess::shell(size)?;
         loop {
-
+            let mut output_future = pin!(pty.read().fuse());
+            let mut input_future = pin!(session.read_object::<TerminalPacket>().fuse());
+            select! {
+                output = output_future => {
+                    match output {
+                        Some(output) => {
+                            session.write_object(&output).await?;
+                        },
+                        None => {return Ok(())}
+                    }
+                },
+                input = input_future => {
+                    match input {
+                        Ok(TerminalPacket::Input(input)) => {
+                            pty.write(input).await;
+                        },
+                        Ok(TerminalPacket::Resize(size)) => {
+                            pty.resize(size);
+                        },
+                        Ok(TerminalPacket::Close) => {
+                            return Ok(());
+                        },
+                        Err(err) => return Err(err),
+                    }
+                },
+            }
         }
-        todo!()
     }
 }
--- svalin_sysctl/src/pty/mod.rs
+++ svalin_sysctl/src/pty/mod.rs
@@ -2,16 +2,18 @@ use std::sync::LazyLock;
 
 use anyhow::Result;
 use portable_pty::{native_pty_system, CommandBuilder, MasterPty, PtySize};
-use tokio::sync::mpsc;
+use serde::{Deserialize, Serialize};
+use tokio::sync::{mpsc, Mutex};
 
 static SHELL: LazyLock<String> = LazyLock::new(|| CommandBuilder::new_default_prog().get_shell());
 
 pub struct PtyProcess {
-    master: Box<dyn MasterPty + Send>,
+    master: Mutex<Box<dyn MasterPty + Send>>,
     write: mpsc::Sender<Vec<u8>>,
-    read: mpsc::Receiver<Vec<u8>>,
+    read: Mutex<mpsc::Receiver<Vec<u8>>>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct TerminalSize {
     cols: u16,
     rows: u16,
@@ -66,17 +68,23 @@ impl PtyProcess {
         });
 
         Ok(Self {
-            master,
-            read: reader_recv,
+            master: Mutex::new(master),
+            read: Mutex::new(reader_recv),
             write: writer_send,
         })
     }
 
-    pub fn resize(&self, rows: u16, cols: u16) -> Result<()> {
-        self.master.resize(PtySize {
-            rows,
-            cols,
-            ..Default::default()
-        })
+    pub async fn resize(&self, size: TerminalSize) -> Result<()> {
+        self.master.lock().await.resize(size.into())
+    }
+
+    pub async fn write(&self, content: Vec<u8>) -> Result<()> {
+        self.write.send(content).await?;
+
+        Ok(())
+    }
+
+    pub async fn read(&self) -> Option<Vec<u8>> {
+        self.read.lock().await.recv().await
     }
 }
