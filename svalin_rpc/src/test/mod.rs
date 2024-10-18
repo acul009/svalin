50deba948c73632f2973d0544d90eb07af2abed4
L. Wlcek <luca@it-rahn.de>
2024-10-19 00:22:40 +0200

    added perm test

--- svalin_rpc/src/test/mod.rs
+++ svalin_rpc/src/test/mod.rs
@@ -8,7 +8,10 @@ mod tls_test_command;
 
 use crate::{
     commands::ping::{Ping, PingHandler},
-    permissions::{anonymous_permission_handler::AnonymousPermissionHandler, DummyPermission},
+    permissions::{
+        anonymous_permission_handler::AnonymousPermissionHandler,
+        whitelist::WhitelistPermissionHandler, DummyPermission,
+    },
     rpc::{
         client::RpcClient, command::handler::HandlerCollection, connection::Connection,
         server::RpcServer,
@@ -16,7 +19,7 @@ use crate::{
     verifiers::skip_verify::{SkipClientVerification, SkipServerVerification},
 };
 
-#[test(tokio::test(flavor = "multi_thread"))]
+#[test(tokio::test)]
 async fn ping_test() {
     println!("starting ping test");
 
@@ -56,7 +59,7 @@ async fn ping_test() {
     server_handle.abort();
 }
 
-#[test(tokio::test(flavor = "multi_thread"))]
+#[test(tokio::test)]
 async fn tls_test() {
     println!("starting tls test");
 
@@ -95,6 +98,49 @@ async fn tls_test() {
     let connection = client.upstream_connection();
 
     connection.dispatch(TlsTest).await.unwrap();
+
+    server_handle.abort();
+}
+
+#[test(tokio::test)]
+async fn perm_test() {
+    println!("starting permission test");
+
+    let address = "127.0.0.1:1236";
+    let credentials = svalin_pki::Keypair::generate()
+        .unwrap()
+        .to_self_signed_cert()
+        .unwrap();
+    let server = RpcServer::new(
+        address.to_socket_addrs().unwrap().next().unwrap(),
+        &credentials,
+        SkipClientVerification::new(),
+    )
+    .unwrap();
+
+    let permission_handler = WhitelistPermissionHandler::new(vec![]);
+
+    let commands = HandlerCollection::<_, DummyPermission>::new(permission_handler);
+    commands
+        .chain()
+        .await
+        .add(TlsTestCommandHandler::new().unwrap());
+
+    let server_handle = tokio::spawn(async move {
+        server.run(commands).await.unwrap();
+    });
+
+    debug!("trying to connect client");
+
+    let client = RpcClient::connect(address, None, SkipServerVerification::new())
+        .await
+        .unwrap();
+
+    debug!("client connected");
+
+    let connection = client.upstream_connection();
+
+    connection.dispatch(TlsTest).await.unwrap_err();
 
     server_handle.abort();
 }
