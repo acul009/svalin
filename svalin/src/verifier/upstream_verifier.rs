839d1399bd23f2385937533d0c46e9c582bd2749
L. Wlcek <luca@it-rahn.de>
2025-01-25 02:21:36 +0100

    added root check for upstream

--- svalin/src/verifier/upstream_verifier.rs
+++ svalin/src/verifier/upstream_verifier.rs
@@ -3,6 +3,7 @@ use svalin_pki::verifier::{exact::ExactVerififier, Verifier};
 /// flutter_rust_bridge:ignore
 #[derive(Debug)]
 pub struct UpstreamVerifier {
+    root_certificate: svalin_pki::Certificate,
     verifier: ExactVerififier,
 }
 
@@ -14,7 +15,10 @@ impl UpstreamVerifier {
         // TODO: verify upstream certificate chain with root
         let verifier = ExactVerififier::new(upstream_certificate);
 
-        Self { verifier }
+        Self {
+            verifier,
+            root_certificate,
+        }
     }
 }
 
@@ -24,6 +28,10 @@ impl Verifier for UpstreamVerifier {
         fingerprint: [u8; 32],
         time: u64,
     ) -> std::result::Result<svalin_pki::Certificate, svalin_pki::verifier::VerificationError> {
-        self.verifier.verify_fingerprint(fingerprint, time).await
+        let cert = self.verifier.verify_fingerprint(fingerprint, time).await?;
+
+        cert.verify_signature(&self.root_certificate)?;
+
+        Ok(cert)
     }
 }
