7461b842a0519c1b667eb92feb09717256a3bf00
L. Wlcek <luca@it-rahn.de>
2025-05-04 00:31:02 +0200

    trying to add font...

--- frostbyte_term/src/main.rs
+++ frostbyte_term/src/main.rs
@@ -1,10 +1,12 @@
 pub mod threaded_writer;
 mod ui;
 
+use iced::Font;
 use ui::UI;
 
 fn main() {
     iced::daemon(UI::title, UI::update, UI::view)
+        .font(include_bytes!("../fonts/3270NerdFontMono-Regular.ttf"))
         .subscription(UI::subscription)
         .theme(|_, _| iced::Theme::Dark)
         .antialiasing(true)
