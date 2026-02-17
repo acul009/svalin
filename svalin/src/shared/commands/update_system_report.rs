01eb76c80890f547fdda26e65be9182be9e2dee8
acul009 <acul009@gmail.com>
2026-02-18 00:57:29 +0100

    need to figure out how to save and deliver this report now

--- svalin/src/shared/commands/update_system_report.rs
+++ svalin/src/shared/commands/update_system_report.rs
@@ -1,6 +1,11 @@
-use svalin_pki::mls::{agent::EncodedReport, client::MlsClient};
-use svalin_rpc::rpc::command::dispatcher::CommandDispatcher;
+use async_trait::async_trait;
+use svalin_pki::mls::{agent::EncodedReport, client::MlsClient, delivery_service::DeliveryService};
+use svalin_rpc::rpc::{
+    command::{dispatcher::CommandDispatcher, handler::CommandHandler},
+    session::Session,
+};
 use svalin_sysctl::sytem_report::SystemReport;
+use tokio_util::sync::CancellationToken;
 
 pub struct UpdateSystemReport(pub EncodedReport<SystemReport>);
 
@@ -23,6 +28,27 @@ impl CommandDispatcher for UpdateSystemReport {
         self,
         session: &mut svalin_rpc::rpc::session::Session,
     ) -> Result<Self::Output, Self::Error> {
+        todo!()
+    }
+}
+
+pub struct UpdateSystemReportHandler {
+    mls: DeliveryService,
+}
+
+#[async_trait]
+impl CommandHandler for UpdateSystemReportHandler {
+    type Request = EncodedReport<SystemReport>;
+
+    fn key() -> String {
+        UpdateSystemReport::key()
+    }
+    async fn handle(
+        &self,
+        session: &mut Session,
+        request: Self::Request,
+        cancel: CancellationToken,
+    ) -> anyhow::Result<()> {
         todo!()
     }
 }
